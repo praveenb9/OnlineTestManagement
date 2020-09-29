b07f9fb35366bba17c8e7cd6c3a269305b47b3d9
Sai Charan <Sai Charan@saicharan>
2020-09-29 12:57:36 +0530

    comit by charan

--- otms-user/src/main/java/com/capg/otms/user/controller/UserController.java
+++ otms-user/src/main/java/com/capg/otms/user/controller/UserController.java
@@ -32,7 +32,7 @@ import com.capg.otms.user.service.IUserService;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	@Autowired
